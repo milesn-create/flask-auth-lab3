a3ca3c8fb2aefe9c3f5788c0745610f42b88a2e5
Anastasia <milessn@MacBook-Air-Anastasia.local>
2026-03-25 22:23:17 +0300

    lab3 auth flask

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+app = Flask(__name__)
+app.secret_key = 'supersecretkey'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+login_manager.login_message = 'Для доступа необходимо войти в систему'
+
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+
+users = {'user': {'password': 'qwerty'}}
+
+@login_manager.user_loader
+def load_user(user_id):
+    if user_id in users:
+        return User(user_id)
+    return None
+
+@app.route('/')
+def index():
+    if 'visits' in session:
+        session['visits'] += 1
+    else:
+        session['visits'] = 1
+
+    return render_template('index.html', visits=session['visits'])
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        remember = True if request.form.get('remember') else False
+
+        if username in users and users[username]['password'] == password:
+            user = User(username)
+            login_user(user, remember=remember)
+            flash('Вы успешно вошли')
+
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('index'))
+        else:
+            flash('Неверный логин или пароль')
+
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/secret')
+@login_required
+def secret():
+    return render_template('secret.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
